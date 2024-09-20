42830a796c159128004aedd40796fb1c4a517522
Kari Lavikka <kari.lavikka@helsinki.fi>
2024-09-20 13:57:17 +0300

    feat: sticky subclone highlighting by clicking

--- src/interactions.ts
+++ src/interactions.ts
@@ -2,13 +2,15 @@ const mutedSubcloneFill = "#f8f8f8";
 const mutedSubcloneStroke = "#d0d0d0";
 const mutedTentacleStroke = "#e8e8e8";
 
+type State = "normal" | "hover" | "clicked";
+
 export function addInteractions(svgElement: SVGElement) {
   const originalColors = new Map<
     SVGElement,
     { fill: string; stroke: string }
   >();
 
-  let hoverActive = false;
+  let state: State = "normal";
 
   for (const elem of svgElement.querySelectorAll(
     ":is(.subclone, .tentacle, .legend-rect, .pass-through)[data-subclone]"
@@ -19,41 +21,51 @@ export function addInteractions(svgElement: SVGElement) {
     });
   }
 
-  svgElement.addEventListener("mouseout", () => {
-    if (hoverActive) {
-      for (const [elem, { fill, stroke }] of originalColors.entries()) {
+  const applyHighlight = (subclones: Set<string> = new Set()) => {
+    for (const [elem, { fill, stroke }] of originalColors.entries()) {
+      if (subclones.has(elem.dataset.subclone) || subclones.size === 0) {
         elem.setAttribute("fill", fill);
         elem.setAttribute("stroke", stroke);
+        if (isTentacle(elem)) {
+          elem.style.mixBlendMode = "normal";
+        }
+      } else {
+        if (isTentacle(elem)) {
+          elem.setAttribute("stroke", mutedTentacleStroke);
+          // Ensure that the highlighted tentacle is not covered by the muted ones
+          elem.style.mixBlendMode = "darken";
+        } else {
+          elem.setAttribute("fill", mutedSubcloneFill);
+          elem.setAttribute("stroke", mutedSubcloneStroke);
+        }
       }
-      hoverActive = false;
+    }
+  };
+
+  svgElement.addEventListener("mouseout", () => {
+    if (state == "hover") {
+      applyHighlight();
     }
   });
 
   svgElement.addEventListener("mouseover", (event) => {
+    if (state === "normal" || state === "hover") {
+      const target = event.target as SVGElement;
+      const subclones = getSubclones(target.dataset);
+      applyHighlight(subclones);
+
+      if (subclones.size > 0) {
+        state = "hover";
+      }
+    }
+  });
+
+  svgElement.addEventListener("click", (event) => {
     const target = event.target as SVGElement;
     const subclones = getSubclones(target.dataset);
+    applyHighlight(subclones);
 
-    if (subclones.size > 0) {
-      for (const [elem, { fill, stroke }] of originalColors.entries()) {
-        if (subclones.has(elem.dataset.subclone)) {
-          elem.setAttribute("fill", fill);
-          elem.setAttribute("stroke", stroke);
-          if (isTentacle(elem)) {
-            elem.style.mixBlendMode = "normal";
-          }
-        } else {
-          if (isTentacle(elem)) {
-            elem.setAttribute("stroke", mutedTentacleStroke);
-            // Ensure that the highlighted tentacle is not covered by the muted ones
-            elem.style.mixBlendMode = "darken";
-          } else {
-            elem.setAttribute("fill", mutedSubcloneFill);
-            elem.setAttribute("stroke", mutedSubcloneStroke);
-          }
-        }
-      }
-      hoverActive = true;
-    }
+    state = subclones.size > 0 ? "clicked" : "normal";
   });
 }
 
@@ -64,7 +76,7 @@ function isTentacle(elem: SVGElement) {
   );
 }
 
-function getSubclones(dataset: DOMStringMap) {
+function getSubclones(dataset: DOMStringMap): Set<string> {
   if (dataset.descendantSubclones) {
     return new Set(JSON.parse(dataset.descendantSubclones));
   } else if (dataset.subclone != null) {

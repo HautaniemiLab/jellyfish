7591e1bdc77d091d4b3c95749c9df43da52cfb15
Kari Lavikka <kari.lavikka@helsinki.fi>
2025-11-20 20:19:01 +0200

    build: fix vite-env
    Something broke down when updating Vite, I guess

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_DATA_DIR: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv;
+  readonly VITE_DATA_DIR?: string;
 }
